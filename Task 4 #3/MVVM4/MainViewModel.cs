a5617fee16fcd7b755d7d83c857a67a6f0a1cde6
ivan01239 <115571433+ivan01239@users.noreply.github.com>
2024-06-02 18:44:02 +0300

    add 4 task

---
+++ Task 4 #3/MVVM4/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+using MVVM4.Models;
+
+namespace MVVM4.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<OilRigViewModel> oilRigs;
+        private Dispatcher dispatcher;
+        private int numberOilRig = 0;
+
+        public ObservableCollection<OilRigViewModel> OilRigs
+        {
+            get { return oilRigs; }
+            set
+            {
+                oilRigs = value;
+                OnPropertyChanged(nameof(OilRigs));
+            }
+        }
+
+        public ICommand AddOilRigCommand { get; }
+
+        public MainViewModel(Dispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+            OilRigs = new ObservableCollection<OilRigViewModel>();
+            AddOilRigCommand = new RelayCommand(async () => await InitializeOilRigsAsync());
+        }
+
+        public async Task InitializeOilRigsAsync()
+        {
+            await Task.Delay(100);
+            OilRigs.Add(new OilRigViewModel(numberOilRig, dispatcher));
+            numberOilRig++;
+            OnPropertyChanged(nameof(numberOilRig));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke() ?? true;
+
+        public void Execute(object parameter) => execute();
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
